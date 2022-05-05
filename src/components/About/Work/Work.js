68ce696c7041fb87e724c29cb5b03e131894d6e6
Brendan Saw <brendansaw@gmail.com>
2022-05-04 21:44:31 -0700

    Updated some text in Work section

--- src/components/About/Work/Work.js
+++ src/components/About/Work/Work.js
@@ -7,7 +7,7 @@ function About() {
     document.title = "Brendan Saw - Work"
     return (
         <div className="Work">
-            <h1>Work</h1>
+            <h1>Work Experience</h1>
             <hr/>
             <h2>Amazon Web Services</h2>
             <section className="section-curved">
@@ -19,8 +19,13 @@ function About() {
                         <li>Work Period: May 2022 - Aug 2022 (4 months)</li>
                         <li>Location: Vancouver, BC</li>
                     </ul>
+                    <b className="text-align-left">Languages:</b>
+                    <ul className="section-curved-ul">
+                        <li>TBD</li>
+                    </ul>
                 </div>
                 <div className="section-curved-vertical-flex section-curved-outer">
+
                     <p><b>Position:</b> Software Development Engineer Intern</p>
                     <div className="section-curved-vertical-flex">
                         <b>Description:</b>
@@ -32,7 +37,7 @@ function About() {
                         <b>During my time at the company, I completed the following:</b>
                         <ul className="section-curved-ul">
                             <li>
-                                TBD.
+                                TBD
                             </li>
                         </ul>
                     </div>
@@ -49,6 +54,29 @@ function About() {
                         <li>Work Period: May 2021 - Dec 2021 (8 months)</li>
                         <li>Location: Burnaby, BC</li>
                     </ul>
+                    <b className="text-align-left">Languages:</b>
+                    <ul className="section-curved-ul">
+                        <li>Python
+                            <ul>
+                                <li>Django</li>
+                                <li>Pandas</li>
+                                <li>NumPy</li>
+                                <li>PyUnit</li>
+                            </ul>
+                        </li>
+                        <li>JavaScript
+                            <ul>
+                                <li>jQuery</li>
+                                <li>React</li>
+                            </ul>
+                        </li>
+                        <li>SQL
+                            <ul>
+                                <li>PostgreSQL</li>
+                                <li>SQL Server</li>
+                            </ul>
+                        </li>
+                    </ul>
                 </div>
                 <div className="section-curved-vertical-flex section-curved-outer">
                     <p><b>Position:</b> Web Developer Co-op</p>
@@ -56,20 +84,8 @@ function About() {
                         <b>Description:</b>
                         <p>
                             I worked at Greenlight Innovation as a Web Developer Co-op. In this position,
-                            I was tasked with writing and testing the main internal web application - the Portal.
-                            This application provided ERP functionality for the company, allowing employees to view
-                            part shortages and examine KPIs. Additionally, the app also served as the main
-                            time-tracking software within the company, and as a result of having all information
-                            stored by the web application, was able to provide analytics on the time-tracking
-                            information to serve generalized reports to managers and executives.
-                        </p>
-
-                        <p>
-                            The Portal is written using the Django templating language with assistance from jQuery for
-                            front-end interactive features. Django's built-in ORM was utilized to serve data from
-                            the PostgreSQL database to the users. Additionally, for any scripts that weren't able to
-                            make use of the templating functionalities, the Django REST Framework was used to provide
-                            the required data.
+                            I was tasked with writing and testing the main internal web application - the Portal,
+                            which provided ERP functionality for the company.
                         </p>
                     </div>
                     <div className="section-curved-vertical-flex">
@@ -92,44 +108,6 @@ function About() {
                             <li>Assisted in manual deployment to production server</li>
                             <li>Assisted in setting up Git and GitLab; pushed for greater usage of PR reviews</li>
                             <li>Led in setting up Jira and Confluence for the team</li>
-                        </ul>
-                    </div>
-                </div>
-            </section>
-            <hr/>
-            <h2>CARO Analytical Services</h2>
-            <section className="section-curved">
-                <div className="section-curved-vertical-flex section-curved-outer">
-                    <img src={CAROLogo} className="work-img-width" alt="Job Logo"/>
-                    <br/>
-                    <b className="text-align-left">Extra Information:</b>
-                    <ul className="section-curved-ul">
-                        <li>Work Period: May 2019 - Aug 2019 (4 months)</li>
-                        <li>Location: Richmond, BC</li>
-                    </ul>
-                </div>
-                <div className="section-curved-vertical-flex section-curved-outer">
-                    <p><b>Position:</b> IT Support & Admin</p>
-                    <div className="section-curved-vertical-flex">
-                        <b>Description:</b>
-                        <p>
-                            I worked at CARO Analytical Services in an IT Support & Admin position. I started working
-                            at CARO in summer after I had graduated from high school. At CARO, I assisted with
-                            general technical issues around the company.
-                        </p>
-                    </div>
-                    <div className="section-curved-vertical-flex">
-                        <b>During my time at the company, I completed the following:</b>
-                        <ul className="section-curved-ul">
-                            <li>Responded to general technical issues using IT dashboard</li>
-                            <li>Wrote script using PowerShell to automatically rename files as per client requests</li>
-                            <li>
-                                Added and removed users from Active Directory, LIMS (Laboratory Information Management System)
-                            </li>
-                            <li>Updated old network diagrams</li>
-                            <li>Set up Multi-Factor Authentication within the company</li>
-                            <li>Set up multiple company devices (monitors, computers, printers)</li>
-                            <li>Assisted in diagnosing server errors with senior IT employee</li>
                         </ul>
                     </div>
                 </div>
